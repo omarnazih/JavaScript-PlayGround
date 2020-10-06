48824fe5be4dcc5b562aef946ea191ee8facdbf1
omar nazih <omarnazihcs@gmail.com>
2020-10-06 11:48:39 +0200

    Break

---
+++ Fast Code/app.js
@@ -0,0 +1,11 @@
+let w;
+w = window.prompt("Enter The number of iterations: ");
+
+//log test
+console.log(w)
+
+// Take the number of iterations
+// Take the input strings
+
+// First, each uppercase or lowercase letter must be shifted -- three -- positions to the -- right --,
+// According to ASCII table: letter 'a' should become letter 'd', letter 'y' must become the character '|' and so on. 
