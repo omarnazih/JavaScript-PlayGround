2c41930229815f069d576821e9050f4f05d1e39a
omar nazih <omarnazihcs@gmail.com>
2020-10-12 15:05:30 +0200

    oop js done

--- OOP-Js/app.js
+++ OOP-Js/app.js
@@ -20,3 +20,45 @@ const ahmed = new person('ahmed', '10-12-1978');
 const fawzi = new person('fawzi', '10-12-1992');
 
 console.log(omar.calculateAge());
+
+class Person{
+
+  constructor(firstName, lastName, dob){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = new Date(dob);
+  }
+
+  getFullName(){
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  calculateAge(){
+    const diff = Date.now() - this.dateOfBirth.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  gotMarried(newLastName){
+    return `${this.firstName} ${newLastName}`;
+  }
+
+}
+
+
+class developer extends Person{
+  constructor(firstName, lastName, skills){
+    super(firstName,lastName);
+
+    this.skills = skills;
+  }
+}
+
+const omar = new Person('omar', 'nazih', '10-12-1998');
+const jesse = new Person('jesse', 'smith', '10-12-1988');
+
+const dev1= new developer('omar', 'nazih', 'js, html, css');
+
+console.log(dev1);
+
+
